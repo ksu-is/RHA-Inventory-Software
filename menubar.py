884f2d837bff348f2fa6e77b882fb78e5e9c4f5c
jpalaci3 <44621551+jpalaci3@users.noreply.github.com>
2018-12-05 11:17:01 -0500

    Create menubar.py
    menu bar

---
+++ menubar.py
@@ -0,0 +1,6 @@
+menubar = Menu(root)
+filemenu = Menu(menubar, tearoff=0)
+filemenu.add_command(label="Account", command=ShowLoginForm)
+filemenu.add_command(label="Exit", command=Exit)
+menubar.add_cascade(label="File", menu=filemenu)
+root.config(menu=menubar)
